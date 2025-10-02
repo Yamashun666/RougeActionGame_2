83dd182bf991187d36e3d1351c8b2282a18c5a2c
Yamashun666 <74637651+Yamashun666@users.noreply.github.com>
2025-10-03 00:09:01 +0900

    スキルの基礎仕様を定義

---
+++ Assets/Scripts/SkillManager.cs
@@ -0,0 +1,37 @@
+using System;
+
+public enum SkillType
+{
+    Attack = 1,
+    Move = 2,
+    Heal = 3,
+    Buff = 4
+}
+public class SkillData
+{
+    public string SkillName;            // スキルの名称
+    public string GroupCode;            // レベルすべてを包括したスキルのCODE
+    public string LevelCode;            // GroupCodeをスキルレベルごとに分割したCODE
+    public int Rarity;                  // このスキルのレアリティ。1=コモン 2=アンコモン 3 = レア 4 = エピック 5 = レジェンダリー
+    public int SkillType001;            // どの効果を反映させるか。SkillTypeに定義。動きを重複させることも可能。
+    public int SkillType002;            // どの効果を反映させるか。SkillTypeに定義。動きを重複させることも可能。
+    public int SkillType003;            // どの効果を反映させるか。SkillTypeに定義。動きを重複させることも可能。
+    public int SkillType004;            // どの効果を反映させるか。SkillTypeに定義。動きを重複させることも可能。
+    public SkillType Type;              // ここにSkillTypeを代入（不要か？）
+    public int EffectAmount001;         // どれだけの効果量を持つか？攻撃力に反映させるなら150でダメージ1.5倍など。どのパラメーターを作用させるかはSkillTypeの中で定義。
+    public int EffectAmount002;         // どれだけの効果量を持つか？攻撃力に反映させるなら150でダメージ1.5倍など。どのパラメーターを作用させるかはSkillTypeの中で定義。
+    public int EffectAmount003;         // どれだけの効果量を持つか？攻撃力に反映させるなら150でダメージ1.5倍など。どのパラメーターを作用させるかはSkillTypeの中で定義。
+    public int CoolTime;                // スキルのCT短縮量。1000分立で表記。
+    public int LevelUP_LevelCode;       // スキルレベルアップ時に渡すLevelCODE
+    public string UseSkillSFX001;       // スキル使用時に再生するSFX。
+    public float DelayUseSkillSFX001;   // UseSkillSFX001の再生遅延量。
+    public string UseSkillSFX002;       // スキル使用時に再生するSFX。
+    public float DelayUseSkillSFX002;   // UseSkillSFX002の再生遅延量。
+    public string UseSkillVFX001;       // スキル使用時に再生するVFX。
+    public float DelayUseSkillVFX001;   // UseSkillVFX001の再生遅延量。
+    public string UseSkillVFX002;       // スキル使用時に再生するVFX。
+    public float DelayUseSkillVFX002;   // UseSkillVFX002の再生遅延量。
+    public string SkillEnhancementTable;// このスキルに渡すスキル成長テーブルの定義。Codeと同じ値を必ず渡すこと
+    public string SkillIcon;            // このスキルのビジュアルアイコン。これのファイル名を検索し、UI上に表記。
+    public string LevelUPSkillCode;     // レベルアップ時に渡すスキルのコード。Nullならレベルアップできる関数を呼ぶときにエラーを吐くようにしろ
+}
