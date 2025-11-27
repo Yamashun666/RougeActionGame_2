cd1ad751255b44f1bacc9dc9df1ae0484ce8aafb
Yamashun666 <74637651+Yamashun666@users.noreply.github.com>
2025-11-28 02:15:18 +0900

    FanctionTypeを少しリファクタリング

--- Assets/Scripts/FactionType.cs
+++ Assets/Scripts/FactionType.cs
@@ -1,8 +1,9 @@
 public enum FactionType
 {
-    Unknown = 0,
-    Player = 1,
-    Enemy = 2,
-    Ally = 3,
-    Neutral = 4,
+    Unknown,
+    Player,
+    Enemy,
+    Ally,
+    Neutral,
+    Count,
 }
